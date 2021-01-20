9737bbac0df57e7280a39038c3b33a9f5393bde7
noah <crashlive112@gmail.com>
2021-01-19 20:26:11 -0600

    commit tree f24eb5dda78f040e9177e871c97fbce1f2e02655

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "First", 11, "M", "Black", "Low", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Second", 12, "F", "White", "High", 0);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Third", 13, "M", "Black", "Low", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Fourth", 14, "F", "White", "High", 0);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Fifth", 15, "M", "Black", "Low", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Sixth", 16, "F", "White", "High", 0);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Seventh", 17, "M", "Black", "Low", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 18, "F", "White", "High", 0);
